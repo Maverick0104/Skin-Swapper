8f4cefb2a8388e561073fff1b24a4a1ecc0a5f4a
Rzy <45715088+Riziebtw@users.noreply.github.com>
2019-09-09 21:25:44 +0200

    Add files via upload

---
+++ Kappa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kappa
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Stopped project, new project: https://shell-industries.net");
+			Console.ReadLine();
+		}
+	}
+}
